e287aaf71919888e5d959eb6469f320d07da308a
MokhtarTouiri <design.solutionts.tn@gmail.com>
2020-07-23 01:38:06 +0000

    fix

---
+++ binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef BINARY_NODE
+#define BINARY_NODE
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Int
+ * @parent: Pointer
+ * @left: left node
+ * @right: right node
+ */
+
+struct binary_tree_s
+
+{
+
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
+
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
