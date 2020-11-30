b78b968a085066ee8695cbc73dbd061fb3cbff54
mokhtartouiri <design.solutions.tn@gmail.com>
2020-11-30 18:02:32 +0100

    fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -11,10 +11,9 @@
 int binary_tree_is_root(const binary_tree_t *node)
 
 {
-int i = 0;
+if (!node->parent)
+return (1);
 if (node == NULL)
-return (i);
-if (!node->left)
-i = 1;
-return (i);
+return (0);
+return (0); 
 }
