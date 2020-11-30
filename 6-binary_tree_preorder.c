83a8e41d6dc74ad6551a6cf6d60890269a7bcbe2
mokhtartouiri <design.solutions.tn@gmail.com>
2020-11-30 18:26:34 +0100

    fix

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * int binary_tree_preorder- through a binary tree using pre-order traversal
+ * @tree:is a pointer to the root node of the tree to traverse
+ * @func:is a pointer to a function to call for each node
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+
+{
+if (!node->parent)
+return (1);
+if (node == NULL)
+return (0);
+return (0);
+}
