3afd421e09423185410062cc8b80e3cc952d1402
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-01 23:32:43 +0100

    fi

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+* @tree:is a pointer to the root node of the tree to traverse
+* Return: 1 if node is a leaf, otherwise 0
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+
+{
+if (tree == NULL)
+return (0);
+if (tree->left || tree->right)
+return ((binary_tree_nodes(tree->left) + 1) +
+	   binary_tree_nodes(tree->right));
+return (binary_tree_nodes(tree->left) +
+	binary_tree_nodes(tree->right));
+return (0);
+}
