e36c3af44e38873b186389394fe89e4f1fa6b781
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-02 00:37:10 +0100

    fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,9 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 {
-if (!node->parent)
-return (NULL);
 if (!node)
+return (NULL);
+if (!node->parent)
 return (NULL);
 if (node->parent->left == node)
 return (node->parent->right);
