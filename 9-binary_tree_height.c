bb369df8078e982dcdbe35cb543c063a32b73d8f
mokhtartouiri <design.solutions.tn@gmail.com>
2020-12-01 17:39:46 +0100

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,10 +11,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 
 {
-size_t a;
-a = 0;
-size_t b;
-b = 0;
+  size_t a, b;
 if (tree == NULL || (!tree->left && !tree->right))
 {
 return (0);
